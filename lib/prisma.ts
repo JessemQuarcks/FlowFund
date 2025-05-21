128ef459865f6f71373be0c2535eb18ee7ae649f
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-05-21 21:38:40 +0000

    Add password field to user table

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prisma = new PrismaClient();
 
-export const prisma = globalThis.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
