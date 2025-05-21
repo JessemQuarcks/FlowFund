65039b098c311b95d80b402c35509c24826f140c
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-05-21 21:39:26 +0000

    Customize next auth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,60 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: "email",
+          label: "Email",
+          placeholder: "example@mail.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("Incorrect username or password");
+        }
+
+        if (!user.password) {
+          throw new Error(
+            "Password-based authentication is not set up for this account"
+          );
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password!
+        );
+        if (!passwordMatch) {
+          throw new Error("Incorrect username or password");
+        }
+
+        return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -18,11 +67,26 @@ export const authOptions: AuthOptions = {
     strategy: "jwt",
   },
   callbacks: {
-    async session({ session, user }) {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id;
+      }
       return session;
     },
     async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
       return token;
     },
   },
 };
+
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, authOptions);
+}
